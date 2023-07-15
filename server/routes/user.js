9aaa3eb04e89c10a92c2b9ba3d0b698bfcffdf1c
Lukman Mahmud <mistamann49@gmail.com>
2023-07-15 20:43:18 +0100

    user validation added

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { createUser } from "../controllers/user.js"
+import { validate, validateUser } from "../middlewares/validator.js"
 
 
 const router = express.Router()
 
-router.post('/create', createUser )
+router.post('/create', validateUser, validate, createUser )
 
 export default router
