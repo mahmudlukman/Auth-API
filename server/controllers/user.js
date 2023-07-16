4e023efa715224aa9516e570b96e7b2bd3c1b70d
Lukman Mahmud <mistamann49@gmail.com>
2023-07-16 21:29:34 +0100

    reset password controller added

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,9 +1,11 @@
 import User from "../model/user.js"
 import VerificationToken from '../model/verificationToken.js'
-import {sendError} from "../utils/helper.js"
+import ResetToken from '../model/resetToken.js'
+import {createRandomBytes, sendError} from "../utils/helper.js"
 import jwt from 'jsonwebtoken'
-import { generateEmailTemplate, generateOTP, mailTransport, plainEmailTemplate } from "../utils/mail.js"
+import { generateEmailTemplate, generateOTP, generatePasswordResetTemplate, mailTransport, plainEmailTemplate } from "../utils/mail.js"
 import { isValidObjectId } from "mongoose"
+import crypto from 'crypto'
 
 export const createUser = async (req, res) => {
   const {name, email, password} = req.body
@@ -77,4 +79,53 @@ export const verifyEmail = async (req, res) => {
     html: plainEmailTemplate("Email Verified Successfully", "Thanks for connecting with us")
   })
   res.json({success: true, message: 'Your email has been verified!', user: {id: user._id, name: user.name, email: user.email}})
+}
+
+export const forgotPassword = async (req, res) => {
+  const {email} = req.body
+  if(!email) return sendError(res, 'Please provide a valid email!')
+
+  const user = await User.findOne({email})
+  if(!user) return sendError(res, 'User not found, invalid request!')
+
+  const token = await ResetToken.findOne({owner: user._id})
+  if(token) return sendError(res, 'Only after one hour you can request for another token!')
+
+  const randomBytes = await createRandomBytes()
+  const resetToken = new ResetToken({owner: user._id, token: randomBytes})
+  await resetToken.save()
+
+  mailTransport().sendMail({
+    from: 'security@email.com',
+    to: user.email,
+    subject: 'Password Reset',
+    html: generatePasswordResetTemplate(`http://localhost:5173/reset-password?token=${randomBytes}&id=${user._id}`)
+  })
+
+  res.json({success: true, message: 'Password reset link has been sent to your email!'})
+}
+
+export const resetPassword = async (req, res) => {
+  const {password} = req.body
+
+  const user = await User.findById(req.user._id)
+  if(!user) return sendError(res, 'user not found')
+
+  const isSamePassword = await user.comparePassword(password)
+  if(isSamePassword) return sendError(res, 'New password must be different from the previous one!')
+
+  if(password.trim().length < 8 || password.trim().length > 20) return sendError(res, 'Password must be 8 to 20 characters long!')
+
+  user.password = password.trim()
+  await user.save()
+
+  await ResetToken.findOneAndDelete({owner: user._id})
+
+  mailTransport().sendMail({
+    from: 'security@email.com',
+    to: user.email,
+    subject: 'Password Reset Successfully',
+    html: plainEmailTemplate('Password Reset Successfully', 'Now you can login with new password'),
+  })
+  res.json({success: true, message: 'Password Reset Successfully'})
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,7 @@
 import express from "express"
-import { createUser, signin, verifyEmail } from "../controllers/user.js"
+import { createUser, forgotPassword, resetPassword, signin, verifyEmail } from "../controllers/user.js"
 import { validate, validateUser } from "../middlewares/validator.js"
+import { isResetTokenValid } from "../middlewares/user.js"
 
 
 const router = express.Router()
@@ -8,5 +9,7 @@ const router = express.Router()
 router.post('/create', validateUser, validate, createUser )
 router.post('/signin', signin )
 router.post('/verify-email', verifyEmail )
+router.post('/forgot-password', forgotPassword )
+router.post('/reset-password', isResetTokenValid, resetPassword )
 
 export default router
